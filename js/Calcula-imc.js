b607ab7a515a8642e9ea7582d4a3fac9cb9fa077
Raniel <r.a_tw@hotmail.com>
2018-07-03 00:36:18 -0300

    boas praticas js

---
+++ js/Calcula-imc.js
@@ -0,0 +1,49 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(i = 0; i< pacientes.length; i++){
+
+	paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	var imc = peso / (altura * altura);
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var alturaEhValida = true;
+	var pesoEhValido = true;
+
+	if(peso <= 0 || peso >= 1000){
+		console.log("peso inválido!");
+		tdImc.textContent = "peso inválido!";
+		pesoEhValido = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(altura <= 0 || altura >= 3.0){
+		console.log("altura inválida!");
+		tdImc.textContent = "altura inválida!";
+		alturaEhValida = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(pesoEhValido && alturaEhValida){
+		imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function calculaImc(peso, altura){
+	var imc = 0;
+	imc = peso / (altura * altura);
+	return imc.toFixed(2);
+}
